3e8c49e44fc4e61f8310991431f5a2a894e9ead4
tahmid-saj <tahmid.saj@gmail.com>
2024-07-12 14:53:21 -0400

    leetcode answers

--- leetcode/python/0253_meeting_rooms_II.py
+++ leetcode/python/0253_meeting_rooms_II.py
@@ -1,5 +1,9 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # return self.mostIntervalIntersections(intervals)
+        return self.heap(intervals)
+
+    def mostIntervalIntersections(self, intervals):
         if len(intervals) <= 1: return 1
         
         minRooms, endPtr = 0, 0
@@ -13,4 +17,20 @@ class Solution:
             if startTimes[i] >= endTimes[endPtr]: endPtr += 1
             else: minRooms += 1
 
+        return minRooms
+    
+    def heap(self, meetings):
+        if len(meetings) <= 1: return 1
+        meetings.sort(key=lambda x: x[0])
+
+        minRooms = 0
+        minHeap = []
+        for meeting in meetings:
+            # remove all meetings that have ended
+            while minHeap and meeting[0] >= minHeap[0][1]:
+                heapq.heappop(minHeap)
+            # add the current meeting into the minHeap
+            heapq.heappush(minHeap, meeting)
+            # all active meetings are in the minHeap, so we need rooms for all of them.
+            minRooms = max(minRooms, len(minHeap))
         return minRooms
