fae36178ac0fd32116dcd9223fab0d76114a5454
tahmid-saj <tahmid.saj@gmail.com>
2024-07-11 22:56:14 -0400

    leetcode answers

--- leetcode/python/0680_valid_palindrome_II.py
+++ leetcode/python/0680_valid_palindrome_II.py
@@ -1,29 +1,15 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         i, j = 0, len(s) - 1
-
-        while i <= j:
-            if s[i] != s[j]:
-                return self.isPalindrome(s, i) or self.isPalindrome(s, j)
-            else:
-                i += 1
-                j -= 1
-        
+        while i < j:
+            if s[i] != s[j]: return self.isPalindrome(s, i + 1, j) or self.isPalindrome(s, i, j - 1)
+            i += 1
+            j -= 1
         return True
     
-    def isPalindrome(self, s, ignore):
-        i, j = 0, len(s) - 1
-
-        while i <= j:
-            if i == ignore: 
-                i += 1
-                continue
-            elif j == ignore: 
-                j -= 1
-                continue
-                
+    def isPalindrome(self, s, i, j):
+        while i < j:
             if s[i] != s[j]: return False
             i += 1
             j -= 1
-        
         return True
