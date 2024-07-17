eb99b22adb73106dcef9f0427c3f7352d0f81478
tahmid-saj <tahmid.saj@gmail.com>
2024-07-17 15:28:27 -0400

    binary tree simulations

--- algo_implementations/python/binary_tree_simulations.py
+++ algo_implementations/python/binary_tree_simulations.py
@@ -1,1 +1,56 @@
+# sorted array to BST using sorted array
+def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+  def helper(l, r):
+    if l > r: return None
+    middle = l + (r - l) // 2
+    node = TreeNode(nums[middle])
+    node.left = helper(l, middle - 1)
+    node.right = helper(middle + 1, r)
+    return node
 
+    return helper(0, len(nums) - 1)
+
+# inorder and preorder arrays to binary tree
+class Solution:
+  def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    inorderNodes = {}
+    for i, v in enumerate(inorder): inorderNodes[v] = i
+    preorderIndex = 0
+
+    def helper(l, r):
+      nonlocal preorderIndex
+      if l > r: return None
+      rootVal = preorder[preorderIndex]
+      node = TreeNode(rootVal)
+      preorderIndex += 1
+      
+      node.left = helper(l, inorderNodes[rootVal] - 1)
+      node.right = helper(inorderNodes[rootVal] + 1, r)
+      return node
+      
+    return helper(0, len(inorder) - 1)
+
+# inorder and postorder arrays to binary tree
+def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+  inorder_nodes = {}
+  for i, v in enumerate(inorder): inorder_nodes[v] = i
+  postorder_index = len(postorder) - 1
+
+  def helper(l, r):
+    nonlocal postorder_index
+    if l > r: return None
+
+    rootVal = postorder[postorder_index]
+    mid = inorder_nodes[rootVal]
+    postorder_index -= 1
+
+    right = helper(mid + 1, r)
+    left = helper(l, mid - 1)
+
+    node = TreeNode(rootVal)
+    node.right = right
+    node.left = left
+
+    return node
+    
+  return helper(0, len(inorder) - 1)
--- ds_implementations/python/hash_map.py
+++ ds_implementations/python/hash_map.py
@@ -1,1 +1,41 @@
 
+class Bucket:
+  def __init__(self):
+    self.bucket = []
+  
+  def update(self, key, value):
+    for i, kv in enumerate(self.bucket):
+      if kv[0] == key:
+        self.bucket[i] = (key, value)
+        return
+    self.bucket.append((key, value))
+
+  def get(self, key):
+    for i, kv in enumerate(self.bucket):
+      if kv[0] == key:
+          return kv[1]
+    return -1
+
+  def remove(self, key):
+    for i, kv in enumerate(self.bucket):
+      if kv[0] == key: del self.bucket[i]
+
+class MyHashMap:
+  def __init__(self):
+    self.keySpace = 2069
+    self.hashMap = [Bucket() for _ in range(self.keySpace)]
+    
+  def hashFunction(self, key):
+    return key % self.keySpace
+
+  def put(self, key: int, value: int) -> None:
+    index = self.hashFunction(key)
+    self.hashMap[index].update(key, value)
+
+  def get(self, key: int) -> int:
+    index = self.hashFunction(key)
+    return self.hashMap[index].get(key)
+
+  def remove(self, key: int) -> None:
+    index = self.hashFunction(key)
+    self.hashMap[index].remove(key)
