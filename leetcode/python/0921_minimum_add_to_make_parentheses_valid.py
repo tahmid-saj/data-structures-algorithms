dcdc34c2d37c53d1c2e4da992e1ae43b3a5aa1fa
tahmid-saj <tahmid.saj@gmail.com>
2024-08-01 18:30:54 -0400

    leetcode answers

--- leetcode/python/0921_minimum_add_to_make_parentheses_valid.py
+++ leetcode/python/0921_minimum_add_to_make_parentheses_valid.py
@@ -1,25 +1,27 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        # return self.greedy(s)
-        return self.stack(s)
+        return self.greedy(s)
+        # return self.stack(s)
 
     def greedy(self, s):
         openPs, closePs = 0, 0
 
         for i in range(len(s)):
-            if s[i] == "(": closePs += 1
-            elif s[i] == ")" and closePs > 0: closePs -= 1
-            else: openPs += 1
+            if s[i] == "(": openPs += 1
+            elif s[i] == ")":
+                if openPs > 0: openPs -= 1
+                else: closePs += 1
         
         return openPs + closePs
     
     def stack(self, s):
         stk = []
-        openPs = 0
+        closePs = 0
 
         for i in range(len(s)):
             if s[i] == "(": stk.append(s[i])
-            elif s[i] == ")" and stk and stk[-1]: stk.pop()
-            else: openPs += 1
+            else:
+                if stk and stk[-1] == "(": stk.pop()
+                else: closePs += 1
         
-        return len(stk) + openPs
+        return len(stk) + closePs
