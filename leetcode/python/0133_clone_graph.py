baed01704b6816cc84fbea84fd84d43b3fdce34d
tahmid-saj <tahmid.saj@gmail.com>
2024-07-31 23:31:29 -0400

    leetcode answers

--- leetcode/python/0133_clone_graph.py
+++ leetcode/python/0133_clone_graph.py
@@ -10,35 +10,44 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if node == None: return None
-        if node.neighbors == None: return Node(node.val, [])
-
-        self.clones = {}
-        # return self.dfs(node)
-        return self.bfs(node)
+        if len(node.neighbors) == 0: return Node(node.val, [])
+        clones = {}
+        # self.iterativeDFS(node, clones)
+        # self.iterativeBFS(node, clones)
+        self.recursiveDFS(node, clones)
+        return clones[node]
     
-    def bfs(self, node):
+    def iterativeDFS(self, node, clones):
+        stk = [node]
+        clones[node] = Node(node.val, [])
+        
+        while stk:
+            node = stk.pop()
+            for neighbor in node.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val, [])
+                    stk.append(neighbor)
+                clones[node].neighbors.append(clones[neighbor])
+    
+    def recursiveDFS(self, node, clones):
+        if not node: return None
+        if node in clones: return clones[node]
+        clones[node] = Node(node.val, [])
+
+        for neighbor in node.neighbors:
+            clone = self.recursiveDFS(neighbor, clones)
+            clones[node].neighbors.append(clone)
+        
+        return clones[node]
+    
+    def iterativeBFS(self, node, clones):
         queue = deque([node])
-        self.clones[node] = Node(node.val, [])
+        clones[node] = Node(node.val, [])
 
         while queue:
-            n = queue.popleft()
-
-            for neighbor in n.neighbors:
-                if neighbor not in self.clones:
-                    self.clones[neighbor] = Node(neighbor.val, [])
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val, [])
                     queue.append(neighbor)
-
-                self.clones[n].neighbors.append(self.clones[neighbor])
-        
-        return self.clones[node]
-    
-    def dfs(self, node):
-        if node == None: return None
-        if node in self.clones: return self.clones[node]
-
-        clone = Node(node.val, [])
-        self.clones[node] = clone
-
-        for i in range(len(node.neighbors)): clone.neighbors.append(self.dfs(node.neighbors[i]))
-        
-        return clone
+                clones[node].neighbors.append(clones[neighbor])
