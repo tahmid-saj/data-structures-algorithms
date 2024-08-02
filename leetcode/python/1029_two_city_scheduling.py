79184d2c6727f7705f6756032342b6b4148076b0
tahmid-saj <tahmid.saj@gmail.com>
2024-08-01 21:04:03 -0400

    leetcode answers

--- leetcode/python/1029_two_city_scheduling.py
+++ leetcode/python/1029_two_city_scheduling.py
@@ -1,5 +1,20 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        return self.greedy1(costs)
+        # return self.greedy2(costs)
+
+    def greedy1(self, costs):
+        costs.sort(key=lambda x: x[0] - x[1])
+
+        cost, i, j = 0, 0, len(costs) - 1
+        while i < j:
+            cost += costs[i][0] + costs[j][1]
+            i += 1
+            j -= 1
+            
+        return cost
+    
+    def greedy2(self, costs):
         costs.sort(key=lambda x: x[0] - x[1])
 
         i, j, res = 0, len(costs) - 1, 0
