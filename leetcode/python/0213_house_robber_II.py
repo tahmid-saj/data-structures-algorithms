2f5eaca525ce171304a5f6debf32662c6fda3b79
tahmid-saj <tahmid.saj@gmail.com>
2024-08-07 16:27:01 -0400

    house robber II

--- leetcode/python/0213_house_robber_II.py
+++ leetcode/python/0213_house_robber_II.py
@@ -1,42 +1,31 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 3: return max(nums)
-        # self.end = False
-        # return self.recursive(nums, len(nums) - 1)
-    
-        return self.dp(nums)
 
-    def recursive(self, nums, index):
-        path1 = nums[index]
-        if index - 2 >= 0:
-            if index == len(nums) - 1: self.end = True
-            path1 += self.recursive(nums, index - 2)
-        path2 = 0
-        if index - 1 >= 0:
-            if index == len(nums) - 1: self.end = False
-            path2 = self.recursive(nums, index - 1)
+        # self.nums = nums
+        # return self.recursive(0, None)
         
-        if index <= 1:
-            if self.end and index == 1: return nums[index]
-            elif self.end and index == 0: return 0
-        
-        if index == 0: return nums[index]
+        return max(
+            self.dp(nums[1:]), self.dp(nums[:-1])
+        )
+
+    @lru_cache(None)
+    def recursive(self, index, start):
+        if index >= len(self.nums): return 0
+
+        path1, path2 = 0, 0
+        if not start or index < len(self.nums) - 1: path1 = self.nums[index] + self.recursive(index + 2, True if index == 0 else start)
+        path2 = self.recursive(index + 1, False if index == 0 else start)
 
         return max(path1, path2)
     
     def dp(self, nums):
-        prev, curr = nums[-1], max(nums[-1], nums[-2])
-        for i in range(len(nums) - 3, 0, -1):
-            tmp = max(nums[i] + prev, curr)
-            prev = curr
-            curr = tmp
-        max1 = curr
+        # below paths are arranged as follows: path2, path1, num where path2 is on the 1st house, path1 is on the 2nd house, num is on the 3rd house
 
-        prev, curr = nums[-2], max(nums[-2], nums[-3])
-        for i in range(len(nums) - 4, -1, -1):
-            tmp = max(nums[i] + prev, curr)
-            prev = curr
-            curr = tmp
-        max2 = curr
-
-        return max(max1, max2)
+        path1, path2 = 0, 0
+        for num in nums:
+            tmp = path1
+            path1 = max(path2 + num, path1)
+            path2 = tmp
+        
+        return path1
