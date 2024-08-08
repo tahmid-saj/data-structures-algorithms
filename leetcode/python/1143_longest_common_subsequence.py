1290adbc71ed9031c622ed7e40b8626159b281a7
tahmid-saj <tahmid.saj@gmail.com>
2024-08-07 20:17:00 -0400

    longest common subsequence

--- leetcode/python/1143_longest_common_subsequence.py
+++ leetcode/python/1143_longest_common_subsequence.py
@@ -7,20 +7,17 @@ class Solution:
         return self.bottomUpLinearSpace(text1, text2)
 
     def recursive(self, text1, text2, index1, index2):
-        # base case: index1 == index2 and index1 < 0: return 0
+        # base case: index1 < 0 or index2 < 0: return 0
         # 1. if text1[index1] == text2[index2]: self.recursive(text1, text2, index1 - 1, index2 - 1)
         # 2. else: call both self.recursive(text1, text2, index1 - 1, index2), self.recursive(text1, text2, index1, index2 - 1)
         if index1 < 0 or index2 < 0: return 0
 
-        res1, res2, res3 = 0, 0, 0
-        if text1[index1] == text2[index2]:
-            res1 = 1 + self.recursive(text1, text2, index1 - 1, index2 - 1)
-        else:
-            res2 = self.recursive(text1, text2, index1 - 1, index2)
-            res3 = self.recursive(text1, text2, index1, index2 - 1)
-        
-        return max(res1, res2, res3)
-    
+        res1, res2 = 0, 0
+        if text1[index1] == text2[index2]: res1 = 1 + self.recursive(text1, text2, index1 - 1, index2 - 1)
+        else: res2 = max(self.recursive(text1, text2, index1 - 1, index2), self.recursive(text1, text2, index1, index2 - 1))
+
+        return max(res1, res2)
+
     def recursive2(self, text1, text2, index1, index2):
         if index1 == len(text1) or index2 == len(text2): return 0
 
@@ -32,8 +29,7 @@ class Solution:
         return max(res1, res2)
     
     def bottomUp(self, text1, text2):
-        if len(text1) == 0 or len(text2) == 0: return 0
-        dp = [[0 for j in range(len(text2))] for i in range(len(text1))]
+        dp = [[0 for _ in range(len(text2))] for _ in range(len(text1))]
 
         for i in range(len(text1)):
             if text2[0] in text1[:i + 1]: dp[i][0] = 1
@@ -60,15 +56,15 @@ class Solution:
     
     def bottomUpLinearSpace(self, text1, text2):
         if len(text1) == 0 or len(text2) == 0: return 0
-        if len(text1) > len(text2): text1, text2 = text2, text1
+        if len(text1) < len(text2): text1, text2 = text2, text1
 
-        prev = [0 for _ in range(len(text1) + 1)]
-
-        for i in range(len(text2) - 1, -1, -1):
-            curr = [0 for _ in range(len(text1) + 1)]
-            for j in range(len(text1) - 1, -1, -1):
-                if text2[i] == text1[j]: curr[j] = prev[j + 1] + 1
+        prev = [0 for _ in range(len(text2) + 1)]
+        
+        for i in range(len(text1) - 1, -1, -1):
+            curr = [0 for _ in range(len(text2) + 1)]
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]: curr[j] = prev[j + 1] + 1
                 else: curr[j] = max(prev[j], curr[j + 1])
             prev = curr
 
-        return prev[0]
+        return curr[0]
