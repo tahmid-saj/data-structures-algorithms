8fb91314cadd4348b25005e41118affe4ae1e01a
tahmid-saj <tahmid.saj@gmail.com>
2024-08-14 22:34:43 -0400

    spiral matrix II

--- leetcode/python/0059_spiral_matrix_II.py
+++ leetcode/python/0059_spiral_matrix_II.py
@@ -1,5 +1,9 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
+        # return self.simulation(n)
+        return self.adjustingDirection(n)
+
+    def simulation(self, n):
         res = [[0 for _ in range(n)] for _ in range(n)]
         row, col, i = 0, 0, 1
         rightBounds = n
@@ -51,5 +55,25 @@ class Solution:
             row += 1
             col += 1
             upBounds += 1
+        
+        return res
+    
+    def adjustingDirection(self, length):
+        m, n = length, length
+        direction, i, j, c, res = 1, 0, -1, 1, [[0 for _ in range(n)] for _ in range(n)]
+
+        while m * n > 0:
+            for _ in range(m):
+                j += direction
+                res[i][j] = c
+                c += 1
+            n -= 1
+
+            for _ in range(n):
+                i += direction
+                res[i][j] = c
+                c += 1
+            m -= 1
+            direction *= -1
         
         return res
