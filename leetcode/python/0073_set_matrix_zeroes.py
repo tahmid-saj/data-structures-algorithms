162bf0b08ce57fb4fbbcdf7884bf0df4adeab98b
tahmid-saj <tahmid.saj@gmail.com>
2024-08-15 15:06:57 -0400

    set matrix zeroes

--- leetcode/python/0073_set_matrix_zeroes.py
+++ leetcode/python/0073_set_matrix_zeroes.py
@@ -5,19 +5,16 @@ class Solution:
         """
         # O(n) space
         # rows, cols = set(), set()
-
         # for i in range(len(matrix)):
         #     for j in range(len(matrix[0])):
         #         if matrix[i][j] == 0:
         #             rows.add(i)
         #             cols.add(j)
         
-        # for r in rows:
-        #     matrix[r] = [0 for _ in range(len(matrix[0]))]
-        
-        # for c in cols:
-        #     for i in range(len(matrix)):
-        #         matrix[i][c] = 0
+        # for i in range(len(matrix)):
+        #     if i in rows: matrix[i] = [0 for _ in range(len(matrix[0]))]
+        #     for j in range(len(matrix[0])):
+        #         if j in cols: matrix[i][j] = 0
 
         # O(1) space
         setFirstColZero = False
