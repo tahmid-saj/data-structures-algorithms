bbd00af0c5669d3f39e6c68512fdeccc533afbae
tahmid-saj <tahmid.saj@gmail.com>
2024-10-19 18:44:33 -0400

    lc 98 modified

--- leetcode/python/0098_validate_binary_search_tree.py
+++ leetcode/python/0098_validate_binary_search_tree.py
@@ -1,80 +1,66 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         # return self.constantSpaceDPDFS(root)
-
         # return self.recursiveLimits(root)
-
         # return self.iterativeLimits(root)
-
         # return self.recursiveInorder(root)
-
         return self.iterativeInorder(root)
 
     def constantSpaceDPDFS(self, root):
         self.dp = []
-
         def helper(node):
-            if node.left and helper(node.left) == False: return False
+            if not node: return
+            if node.left and not helper(node.left): return False
 
-            self.dp.append(node)
-            if len(self.dp) == 2 and self.dp[-2].val >= self.dp[-1].val: return False
-            if len(self.dp) == 2: self.dp.pop(0)
+            self.dp.append(node.val)
+            if len(self.dp) > 2: self.dp.pop(0)
+            if len(self.dp) == 2 and self.dp[0] >= self.dp[1]: return False
 
-            if node.right and helper(node.right) == False: return False
-
+            if node.right and not helper(node.right): return False
             return True
-
-        if helper(root) == False: return False
-        return True
-    
+        
+        return helper(root)
+        
     def recursiveLimits(self, root):
-        def helper(node, low=-math.inf, high=math.inf):
-            if node == None: return True
+        def helper(node, lLimit, rLimit):
+            if not node: return
+            if node.left and not helper(node.left, lLimit, node.val - 1): return False
 
-            if node.val <= low or node.val >= high: return False
+            if not (lLimit <= node.val <= rLimit): return False
 
-            if helper(node.left, low, node.val) == False: return False
-            if helper(node.right, node.val, high) == False: return False
+            if node.right and not helper(node.right, node.val + 1, rLimit): return False
+            return True
+        
+        return helper(root, -math.inf, math.inf)
 
-        if helper(root) == False: return False
-        return True
-    
     def iterativeLimits(self, root):
-        if root == None: return True
-
-        stk = []
-        stk.append((root, -math.inf, math.inf))
+        stk = [(root, -math.inf, math.inf)]
 
         while stk:
-            node, low, high = stk.pop()
+            node, lLimit, rLimit = stk.pop()
+            if not (lLimit <= node.val <= rLimit): return False
 
-            if node.val <= low or node.val >= high: return False
-            if node.left: stk.append((node.left, low, node.val))
-            if node.right: stk.append((node.right, node.val, high))
-
+            if node.right: stk.append((node.right, node.val + 1, rLimit))
+            if node.left: stk.append((node.left, lLimit, node.val - 1))
+        
         return True
-    
+
     def recursiveInorder(self, root):
         self.prev = -math.inf
         def helper(node):
-            if node.left and helper(node.left) == False: return False
+            if not node: return
+            if node.left and not helper(node.left): return False
 
-            if node.val <= self.prev: return False
+            if not (self.prev < node.val): return False 
             self.prev = node.val
 
-            if node.right and helper(node.right) == False: return False
+            if node.right and not helper(node.right): return False
+            return True
+        
+        return helper(root)
 
-        if helper(root) == False: return False
-        return True
-    
     def iterativeInorder(self, root):
-        stk, prev = [], -math.inf
+        prev, stk = -math.inf, []
 
         while stk or root:
             while root:
@@ -82,7 +68,7 @@ class Solution:
                 root = root.left
             root = stk.pop()
 
-            if root.val <= prev: return False
+            if not (prev < root.val): return False
             prev = root.val
             root = root.right
         
