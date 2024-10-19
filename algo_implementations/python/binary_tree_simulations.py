f47ecf2983e7531e5d12ef2dd2e6514e487d66c4
tahmid-saj <tahmid.saj@gmail.com>
2024-10-19 17:22:55 -0400

    binary tree simulations

--- algo_implementations/python/binary_tree_simulations.py
+++ algo_implementations/python/binary_tree_simulations.py
@@ -1,17 +1,112 @@
 # sorted array to BST using sorted array
-def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-  def helper(l, r):
-    if l > r: return None
-    middle = l + (r - l) // 2
-    node = TreeNode(nums[middle])
-    node.left = helper(l, middle - 1)
-    node.right = helper(middle + 1, r)
-    return node
+class Solution:
+  def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+    def helper(l, r):
+      if l > r: return None
+      middle = l + (r - l) // 2
+      node = TreeNode(nums[middle])
+      node.left = helper(l, middle - 1)
+      node.right = helper(middle + 1, r)
+      return node
 
-  return helper(0, len(nums) - 1)
+    return helper(0, len(nums) - 1)
 
 # sorted linked list to binary search tree
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        # return self.recursiveArray(head)
+        # return self.recursiveArray2(head)
+        # return self.recursive(head)
+        return self.inorderSimulation(head)
+    
+    def recursiveArray(self, head):
+        if head == None: return None
+        nodes = []
+        while head:
+            nodes.append(head.val)
+            head = head.next
+        
+        def helper(nodes):
+            mid = len(nodes) // 2
+            if len(nodes) == 0: return None
+            middle = TreeNode(nodes[mid])
 
+            middle.left = helper(nodes[:mid])
+            middle.right = helper(nodes[mid + 1:])
+
+            return middle
+
+        return helper(nodes)
+    
+    def recursiveArray2(self, head):
+        if head == None: return None
+        nodes = []
+        while head:
+            nodes.append(head.val)
+            head = head.next
+        
+        def helper(l, r):
+            if l > r: return None
+            mid = (l + r) // 2
+            middle = TreeNode(nodes[mid])
+            if l == r: return middle
+
+            middle.left = helper(l, mid - 1)
+            middle.right = helper(mid + 1, r)
+
+            return middle
+
+        return helper(0, len(nodes) - 1)
+    
+    def recursive(self, head):
+        if head == None: return None
+
+        def findMiddle(start):
+            prev, slow, fast = None, start, start
+
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+            
+            if prev: prev.next = None
+
+            return slow
+
+        mid = findMiddle(head)
+        middle = TreeNode(mid.val)
+        if head == mid: return middle
+
+        middle.left = self.recursive(head)
+        middle.right = self.recursive(mid.next)
+
+        return middle
+    
+    def inorderSimulation(self, head):
+        if head == None: return None
+        length, n = 0, head
+        while n:
+            length += 1
+            n = n.next
+        
+        def helper(l, r):
+            nonlocal head
+            if l > r: return None
+
+            mid = (l + r) // 2
+
+            left = helper(l, mid - 1)
+
+            middle = TreeNode(head.val)
+            middle.left = left
+            head = head.next
+
+            middle.right = helper(mid + 1, r)
+
+            return middle
+        
+        return helper(0, length - 1)
+      
 # inorder and preorder arrays to binary tree
 class Solution:
   def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
