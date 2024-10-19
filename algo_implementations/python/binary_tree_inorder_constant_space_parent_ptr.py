44b7f0c083007e054f439cb3251c34607cef0f19
tahmid-saj <tahmid.saj@gmail.com>
2024-10-19 14:22:37 -0400

    binary tree all traversals

--- algo_implementations/python/binary_tree_inorder_constant_space_parent_ptr.py
+++ algo_implementations/python/binary_tree_inorder_constant_space_parent_ptr.py
@@ -1,1 +1,20 @@
+class Solution:
+  def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    return self.constantSpaceParentPointers(root)
+    
+  def constantSpaceParentPointers(self, root):
+    res, prev = [], None
 
+    while root:
+      if root.parent == prev:
+        if root.left: next = root.left
+        else:
+          res.append(root.val)
+          next = root.right or root.parent
+      elif root.left == prev:
+        res.append(root.val)
+        next = root.right or root.parent
+      else: next = root.parent
+      root, prev = next, root
+
+    return res
