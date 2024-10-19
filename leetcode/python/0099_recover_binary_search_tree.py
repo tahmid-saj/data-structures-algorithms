6c2314417f8757173901f50b30876d3865351922
tahmid-saj <tahmid.saj@gmail.com>
2024-10-19 19:27:37 -0400

    lc 99 modified

--- leetcode/python/0099_recover_binary_search_tree.py
+++ leetcode/python/0099_recover_binary_search_tree.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
@@ -11,8 +5,8 @@ class Solution:
         """
         # sorted array from inorder traversal
         # self.res = []
-        # def helper(node):
-        #     if node.left and helper(node.left): return True
+        # def isBST(node):
+        #     if node.left and isBST(node.left): return True
 
         #     self.res.append(node)
         #     if len(self.res) > 2:
@@ -26,76 +20,77 @@ class Solution:
         #             self.res[-1].val, self.res[-2].val = self.res[-2].val, self.res[-1].val
         #             return True
             
-        #     if node.right and helper(node.right): return True
+        #     if node.right and isBST(node.right): return True
 
         #     return False
 
-        # if not helper(root) and len(self.res) == 2 and self.res[-1].val < self.res[-2].val: self.res[-1].val, self.res[-2].val = self.res[-2].val, self.res[-1].val
+        # if not isBST(root) and len(self.res) == 2 and self.res[-1].val < self.res[-2].val: self.res[-1].val, self.res[-2].val = self.res[-2].val, self.res[-1].val
 
         # recursive inorder traversal
         # self.first = None
         # self.second = None
         # self.prev = None
         # def helper(node):
+        #     if not node: return
         #     if node.left: helper(node.left)
 
-        #     if self.prev != None and node.val < self.prev.val:
-        #         if self.first == None: self.first = self.prev
+        #     if self.prev and self.prev.val > node.val:
+        #         if not self.first: self.first = self.prev
         #         self.second = node
             
         #     self.prev = node
 
         #     if node.right: helper(node.right)
-
+        
         # helper(root)
         # self.first.val, self.second.val = self.second.val, self.first.val
 
         # iterative inorder traversal
-        # first = None
-        # second = None
-        # prev = None
-        # stk = []
-        # while stk or root:
-        #     while root:
-        #         stk.append(root)
-        #         root = root.left
-        #     root = stk.pop()
+        prev = None
+        first = None
+        second = None
+        stk = []
 
-        #     if prev and root.val < prev.val:
-        #         second = root
-        #         if first == None: first = prev
-        #         else: break
-        #     prev = root
-        #     root = root.right
+        while stk or root:
+            while root:
+                stk.append(root)
+                root = root.left
+            root = stk.pop()
 
-        # first.val, second.val = second.val, first.val
-
-        # inorder morris traversal
-        first = second = prev = None
-        pred, curr = None, root
-
-        while curr:
-            if curr.left == None:
-                if prev and curr.val < prev.val:
-                    second = curr
-                    if first == None: first = prev
-                prev = curr
-                
-                curr = curr.right
-            else:
-                pred = curr.left
-                while pred.right and pred.right != curr: pred = pred.right
-
-                if pred.right == None:
-                    pred.right = curr
-                    curr = curr.left
-                else:
-                    if prev and curr.val < prev.val:
-                        second = curr
-                        if first == None: first = prev
-                    prev = curr
-
-                    pred.right = None
-                    curr = curr.right
+            if prev and prev.val > root.val:
+                if not first: first = prev
+                second = root
+            prev = root
+            root = root.right
         
         first.val, second.val = second.val, first.val
+
+        # inorder morris traversal
+        # first = second = prev = None
+        # pred, curr = None, root
+
+        # while curr:
+        #     if curr.left == None:
+        #         if prev and curr.val < prev.val:
+        #             second = curr
+        #             if first == None: first = prev
+        #         prev = curr
+                
+        #         curr = curr.right
+        #     else:
+        #         pred = curr.left
+        #         while pred.right and pred.right != curr: pred = pred.right
+
+        #         if pred.right == None:
+        #             pred.right = curr
+        #             curr = curr.left
+        #         else:
+        #             if prev and curr.val < prev.val:
+        #                 second = curr
+        #                 if first == None: first = prev
+        #             prev = curr
+
+        #             pred.right = None
+        #             curr = curr.right
+        
+        # first.val, second.val = second.val, first.val
