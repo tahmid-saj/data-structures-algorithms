4b6f3d885539ad4850052f692bf31ca436613d65
tahmid-saj <tahmid.saj@gmail.com>
2024-10-19 22:28:10 -0400

    lc 130 modified

--- leetcode/python/0130_surrounded_regions.py
+++ leetcode/python/0130_surrounded_regions.py
@@ -12,26 +12,28 @@ class Solution:
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == "O": board[i][j] = "X"
-                elif board[i][j] == "E": board[i][j] = "O"
+                if board[i][j] == "E": board[i][j] = "O"
+                elif board[i][j] == "O": board[i][j] = "X"
     
     def dfs(self, board, i, j):
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]): return
         if board[i][j] != "O": return
         board[i][j] = "E"
 
-        if i > 0: self.dfs(board, i - 1, j)
-        if i < len(board) - 1: self.dfs(board, i + 1, j)
-        if j > 0: self.dfs(board, i, j - 1)
-        if j < len(board[0]) - 1: self.dfs(board, i, j + 1)
+        self.dfs(board, i - 1, j)
+        self.dfs(board, i + 1, j)
+        self.dfs(board, i, j - 1)
+        self.dfs(board, i, j + 1)
     
     def bfs(self, board, i, j):
         queue = deque([(i, j)])
+
         while queue:
             i, j = queue.popleft()
             if board[i][j] != "O": continue
             board[i][j] = "E"
 
-            if i > 0: queue.append((i - 1, j))
-            if i < len(board) - 1: queue.append((i + 1, j))
-            if j > 0: queue.append((i, j - 1))
-            if j < len(board[0]) - 1: queue.append((i, j + 1))
+            if i - 1 > 0: queue.append((i - 1, j))
+            if i + 1 < len(board) - 1: queue.append((i + 1, j))
+            if j - 1 > 0: queue.append((i, j - 1))
+            if j + 1 < len(board[0]) - 1: queue.append((i, j + 1))
