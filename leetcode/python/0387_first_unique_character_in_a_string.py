1df431e5da9a1bdbc7385e56514115221c89faa0
tahmid-saj <tahmid.saj@gmail.com>
2024-10-20 02:18:21 -0400

    lc 387 modified

--- leetcode/python/0387_first_unique_character_in_a_string.py
+++ leetcode/python/0387_first_unique_character_in_a_string.py
@@ -2,10 +2,12 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         freq = {}
         for i in range(len(s)):
-            if s[i] not in freq: freq[s[i]] = (i, 1)
-            else: freq[s[i]]= (freq[s[i]][0], freq[s[i]][1] + 1)
+            if s[i] in freq: freq[s[i]] = (freq[s[i]][0], freq[s[i]][1] + 1)
+            else: freq[s[i]] = (i, 1)
+
+        res = math.inf
+        for k, v in freq.items():
+            if v[1] == 1: res = min(res, v[0])
         
-        for i, v in freq.items():
-            if v[1] == 1: return v[0]
-        
-        return -1
+        if res == math.inf: return -1
+        return res
