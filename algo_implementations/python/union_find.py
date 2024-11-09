54de99329b3f8171093aedb5ff3e3516862a56d3
tahmid-saj <tahmid.saj@gmail.com>
2024-11-08 20:16:13 -0500

    union find

--- algo_implementations/python/union_find.py
+++ algo_implementations/python/union_find.py
@@ -1,1 +1,36 @@
+class UnionFind:
+  def __init__(self):
+    self.parent = []
+  
+  def find(self, i):
+    if self.parent[i] == i: return i
+    return self.find(self.parent[i])
+  
+  def union(self, i, j):
+    iRep = self.find(i)
+    jRep = self.find(j)
+    
+    if iRep == jRep: return
+    
+    self.parent[iRep] = jRep
 
+class UnionFindOptimized:
+  def __init__(self, n):
+    self.rank = [1 for _ in range(n)]
+    self.parent = [i for i in range(n)]
+  
+  def find(self, x):
+    if self.parent[x] != x: self.parent[x] = self.find(self.parent[x])
+    return self.parent[x]
+
+  def union(self, x, y):
+    xSet = self.find(x)
+    ySet = self.find(y)
+    
+    if xSet == ySet: return
+    
+    if self.rank[xSet] < self.rank[ySet]: self.parent[xSet] = ySet
+    elif self.rank[xSet] > self.rank[ySet]: self.parent[ySet] = xSet
+    else:
+      self.parent[ySet] = xSet
+      self.rank[xSet] += 1
