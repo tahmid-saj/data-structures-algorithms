62186764dafb4bd2560bbbd2b7eb6e3da9045e15
tahmid-saj <tahmid.saj@gmail.com>
2024-11-08 21:33:01 -0500

    lc 684 updated

--- leetcode/python/0684_redundant_connection.py
+++ leetcode/python/0684_redundant_connection.py
@@ -1,29 +1,46 @@
-class DSU:
+class DSUOptimized:
     def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.size = [0 for i in range(n)]
+        self.rank = {i:1 for i in range(1, n + 1)}
+        self.parent = {i:i for i in range(1, n + 1)}
     
     def find(self, i):
         if self.parent[i] != i: self.parent[i] = self.find(self.parent[i])
         return self.parent[i]
     
     def union(self, i, j):
-        iSet, jSet = self.find(i), self.find(j)
-        if iSet == jSet: return
+        iRep = self.find(i)
+        jRep = self.find(j)
 
-        if self.size[iSet] < self.size[jSet]: self.parent[iSet] = jSet
-        elif self.size[iSet] > self.size[jSet]: self.parent[jSet] = iSet
+        if iRep == jRep: return
+
+        if self.rank[iRep] < self.rank[jRep]: self.parent[iRep] = jRep
+        elif self.rank[iRep] > self.rank[jRep]: self.parent[jRep] = iRep
         else:
-            self.size[jSet] += 1
-            self.parent[iSet] = jSet
+            self.parent[jRep] = iRep
+            self.rank[iRep] += 1
+
+class DSU:
+    def __init__(self, n):
+        self.parent = {i:i for i in range(1, n + 1)}
+    
+    def find(self, i):
+        if self.parent[i] != i: return self.find(self.parent[i])
+        return self.parent[i]
+    
+    def union(self, i, j):
+        iSet, jSet = self.find(i), self.find(j)
+
+        if iSet == jSet: return
+        self.parent[iSet] = jSet
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        dsu = DSU(len(edges) + 1)
+        dsu = DSUOptimized(len(edges) + 1)
+        # dsu = DSU(len(edges) + 1)
+        res = None
 
         for u, v in edges:
-            if dsu.find(u) != dsu.find(v):
-                dsu.union(u, v)
-            else: return [u, v]
+            if dsu.find(u) != dsu.find(v): dsu.union(u, v)
+            else: res = [u, v]
         
-        return [-1, -1]
+        return res
