3fafc1b0d89a6fc008831bff89624f286acc3035
tahmid-saj <tahmid.saj@gmail.com>
2024-11-09 01:10:53 -0500

    ordered set

--- ds_implementations/python/ordered_set.py
+++ ds_implementations/python/ordered_set.py
@@ -1,1 +1,17 @@
+class OrderedSet:
+  def __init__(self):
+    self.set = set()
+    self.nums = []
+  
+  def add(self, x):
+    if x not in self.set:
+      self.set.add(x)
+      self.nums.append(x)
+      self.nums.sort()
+  
+  def findByOrder(self, i):
+    if i < 0 or i >= len(self.set): return None
+    return self.nums[i]
 
+  def orderOfKey(self, x):
+    return sum(1 for val in self.nums if val < x)
