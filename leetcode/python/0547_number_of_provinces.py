9f4857a8843cf339f021f54d4ab8641b46932f50
tahmid-saj <tahmid.saj@gmail.com>
2024-11-08 22:20:30 -0500

    lc 547 updated

--- leetcode/python/0547_number_of_provinces.py
+++ leetcode/python/0547_number_of_provinces.py
@@ -1,63 +1,80 @@
-class DSU:
+class DSUOptimized:
     def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.size = [0 for i in range(n)]
+        self.rank = {i:1 for i in range(n)}
+        self.parent = {i:i for i in range(n)}
+        self.provinces = n
     
     def find(self, i):
         if self.parent[i] != i: self.parent[i] = self.find(self.parent[i])
         return self.parent[i]
     
     def union(self, i, j):
+        iRep = self.find(i)
+        jRep = self.find(j)
+        if iRep == jRep: return
+
+        if self.rank[iRep] < self.rank[jRep]: self.parent[iRep] = jRep
+        elif self.rank[iRep] > self.rank[jRep]: self.parent[jRep] = iRep
+        else:
+            self.parent[iRep] = jRep
+            self.rank[jRep] += 1
+        self.provinces -= 1
+
+class DSU:
+    def __init__(self, n):
+        self.parent = {i:i for i in range(n)}
+        self.provinces = n
+    
+    def find(self, i):
+        if self.parent[i] != i: return self.find(self.parent[i])
+        return self.parent[i]
+    
+    def union(self, i, j):
         iSet, jSet = self.find(i), self.find(j)
         if iSet == jSet: return
-
-        if self.size[i] < self.size[j]: self.parent[iSet] = jSet
-        elif self.size[i] > self.size[j]: self.parent[jSet] = iSet
-        else:
-            self.size[jSet] += 1
-            self.parent[iSet] = jSet
+        self.parent[iSet] = jSet
+        self.provinces -= 1
 
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # provinces = 0
-        # visited = set()
-        # for i in range(len(isConnected)):
-        #     if i not in visited:
-        #         # self.stack(isConnected, visited, i)
-        #         self.dfs(isConnected, visited, i)
-        #         provinces += 1
+        res = 0
+        visited = set()
+
+        for i in range(len(isConnected)):
+            if i not in visited:
+                res += 1
+                # self.dfs(isConnected, visited, i)
+                self.dfsStk(isConnected, visited, i)
         
-        # return provinces
+        return res
 
-        return self.unionFind(isConnected)
+        # return self.unionFind(isConnected)
 
-    def stack(self, isConnected, visited, city):
+    def dfsStk(self, isConnected, visited, city):
         stk = [city]
         visited.add(city)
 
         while stk:
-            city = stk.pop()
+            i = stk.pop()
 
-            for j in range(len(isConnected[city])):
-                if isConnected[city][j] == 1 and j != city and j not in visited:
+            for j in range(len(isConnected[i])):
+                if i != j and isConnected[i][j] == 1 and j not in visited: 
                     visited.add(j)
                     stk.append(j)
     
     def dfs(self, isConnected, visited, i):
         visited.add(i)
 
-        for j in range(len(isConnected[i])):
-            if i != j and isConnected[i][j] == 1 and j not in visited:
-                self.dfs(isConnected, visited, j)
+        for j in range(len(isConnected[0])):
+            if i != j and isConnected[i][j] == 1 and j not in visited: self.dfs(isConnected, visited, j)
     
     def unionFind(self, isConnected):
-        provinces = len(isConnected)
-        dsu = DSU(provinces + 1)
+        dsu = DSUOptimized(len(isConnected))
+        # dsu = DSU(len(isConnected))
+        res = 0
 
-        for u in range(len(isConnected)):
-            for v in range(len(isConnected[0])):
-                if isConnected[u][v] == 1 and dsu.find(u) != dsu.find(v):
-                    dsu.union(u, v)
-                    provinces -= 1
-
-        return provinces
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[0])):
+                if i != j and isConnected[i][j] == 1 and dsu.find(i) != dsu.find(j): dsu.union(i, j)
+        
+        return dsu.provinces
