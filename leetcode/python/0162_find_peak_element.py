56e31ed1dca5fd0b217c148eccb483b5cc995dc8
tahmid-saj <tahmid.saj@gmail.com>
2024-11-25 01:28:54 -0500

    lc 162 updated

--- leetcode/python/0162_find_peak_element.py
+++ leetcode/python/0162_find_peak_element.py
@@ -4,18 +4,19 @@ class Solution:
         return self.recursive(nums, 0, len(nums) - 1)
 
     def iterative(self, nums):
+        if len(nums) == 1: return 0
         l, r = 0, len(nums) - 1
 
         while l < r:
             middle = l + (r - l) // 2
-            
-            if middle > 0 and middle < len(nums) - 1 and nums[middle] > nums[middle - 1] and nums[middle] > nums[middle + 1]: return middle
+
+            if middle > 0 and middle < len(nums) - 1 and nums[middle - 1] < nums[middle] > nums[middle + 1]: return middle
             elif middle == 0 and middle < len(nums) - 1 and nums[middle] > nums[middle + 1]: return middle
             elif middle == len(nums) - 1 and middle  > 0 and nums[middle] > nums[middle - 1]: return middle
 
             if nums[middle] < nums[middle + 1]: l = middle + 1
             else: r = middle
-        
+
         return l
 
     def recursive(self, nums, l, r):
