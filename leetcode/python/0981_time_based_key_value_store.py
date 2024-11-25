3c300ed38e2924715d012f695ab66cb50cdbee21
tahmid-saj <tahmid.saj@gmail.com>
2024-11-25 17:40:31 -0500

    lc 981 updated

--- leetcode/python/0981_time_based_key_value_store.py
+++ leetcode/python/0981_time_based_key_value_store.py
@@ -1,26 +1,29 @@
+from collections import defaultdict
 from sortedcontainers import SortedDict
 class TimeMap:
 
     def __init__(self):
         # self.map = defaultdict(list)
+        
         self.map = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # self.map[key].append((timestamp, value))
+        # self.hashMap[key].append((timestamp, value))
+
         if key not in self.map: self.map[key] = SortedDict()
         self.map[key][timestamp] = value
 
     def get(self, key: str, timestamp: int) -> str:
-        # if key not in self.map: return ""
-        # l, r = 0, len(self.map[key])
-        # if r == -1: return ""
-        # if timestamp < self.map[key][0][0]: return ""
+        # l, r, res = 0, len(self.hashMap[key]) - 1, ""
 
-        # while l < r:
+        # while l <= r:
         #     middle = l + (r - l) // 2
-        #     if self.map[key][middle][0] <= timestamp: l = middle + 1
-        #     else: r = middle
-        # return "" if r == 0 else self.map[key][l - 1][1]
+        #     if self.hashMap[key][middle][0] <= timestamp: res = self.hashMap[key][middle][1]
+
+        #     if self.hashMap[key][middle][0] < timestamp: l = middle + 1
+        #     else: r = middle - 1
+        
+        # return res
         
         if key not in self.map: return ""
         i = self.map[key].bisect_right(timestamp)
