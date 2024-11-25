440ab5ce3be380925fba70a89821515a3447455e
tahmid-saj <tahmid.saj@gmail.com>
2024-11-25 01:46:40 -0500

    lc 165 updated

--- leetcode/python/0165_compare_version_numbers.py
+++ leetcode/python/0165_compare_version_numbers.py
@@ -4,19 +4,16 @@ class Solution:
         return self.withoutSplitting(version1, version2)
 
     def splitting(self, version1, version2):
-        version1 = version1.split(".")
-        version2 = version2.split(".")
+        version1Split = version1.split(".")
+        version2Split = version2.split(".")
         i, j = 0, 0
-        while i < len(version1) or j < len(version2):
-            num1, num2 = 0, 0
-            if i < len(version1): num1 = int(version1[i])
-            if j < len(version2): num2 = int(version2[i])
-
-            if num1 < num2: return -1
-            if num1 > num2: return 1
+        while i < len(version1Split) or j < len(version2Split):
+            ver1 = int(version1Split[i]) if i < len(version1Split) else 0
+            ver2 = int(version2Split[j]) if j < len(version2Split) else 0
+            if ver1 < ver2: return -1
+            elif ver1 > ver2: return 1
             i += 1
             j += 1
-
         return 0
 
     def withoutSplitting(self, version1, version2):
