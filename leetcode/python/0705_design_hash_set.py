c0ca88b949b0fc225e33a8d346912df32773c237
tahmid-saj <tahmid.saj@gmail.com>
2024-11-25 16:14:25 -0500

    lc 705 updated

--- leetcode/python/0705_design_hash_set.py
+++ leetcode/python/0705_design_hash_set.py
@@ -1,53 +1,59 @@
 class Node:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
 class Bucket:
     def __init__(self):
-        self.head = Node(0)
+        self.head = Node(-1, None)
     
-    def add(self, key):
-        if not self.exists(key):
-            newNode = Node(val=key, next=self.head.next)
-            self.head.next = newNode
-            return
-
+    def update(self, key):
+        existingNode = self.contains(key)
+        if not existingNode:
+            node = Node(key, None)
+            firstNode = self.head.next
+            self.head.next = node
+            node.next = firstNode
+        else:
+            existingNode.val = key
+    
     def remove(self, key):
         prev, curr = self.head, self.head.next
         while curr:
-            if curr.val == key: 
+            if curr.val == key:
                 prev.next = curr.next
+                del curr
                 return
             prev = curr
             curr = curr.next
     
-    def exists(self, key):
+    def contains(self, key):
         curr = self.head.next
         while curr:
-            if curr.val == key: return True
+            if curr.val == key: return curr
             curr = curr.next
-        return False
+        return None
 
 class MyHashSet:
     def __init__(self):
-        self.keySpace = 769
-        self.set = [Bucket() for _ in range(self.keySpace)]
+        self.keySpace = 2069
+        self.hashSet = [Bucket() for _ in range(self.keySpace)]
     
     def hashFunction(self, key):
         return key % self.keySpace
 
     def add(self, key: int) -> None:
-        index = self.hashFunction(key)
-        self.set[index].add(key)
+        hashKey = self.hashFunction(key)
+        self.hashSet[hashKey].update(key)
 
     def remove(self, key: int) -> None:
-        index = self.hashFunction(key)
-        self.set[index].remove(key)
+        hashKey = self.hashFunction(key)
+        return self.hashSet[hashKey].remove(key)
 
     def contains(self, key: int) -> bool:
-        index = self.hashFunction(key)
-        return self.set[index].exists(key)
+        hashKey = self.hashFunction(key)
+        if self.hashSet[hashKey].contains(key): return True
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
