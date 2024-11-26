85da67dfc7598e6f8de6a21b31f94270b81a25b7
tahmid-saj <tahmid.saj@gmail.com>
2024-11-26 15:50:05 -0500

    lc 208 updated

--- leetcode/python/0208_implement_trie.py
+++ leetcode/python/0208_implement_trie.py
@@ -4,36 +4,34 @@ class TrieNode:
         self.isEnd = False
 
 class Trie:
-
     def __init__(self):
         self.root = TrieNode()
-
+    
     def charToIndex(self, ch):
         return ord(ch) - ord('a')
 
     def insert(self, word: str) -> None:
         node = self.root
         for w in word:
-            i = self.charToIndex(w)
-            if i not in node.children: node.children[i] = TrieNode()
-            node = node.children[i]
+            index = self.charToIndex(w)
+            if index not in node.children: node.children[index] = TrieNode()
+            node = node.children[index]
         node.isEnd = True
 
     def search(self, word: str) -> bool:
         node = self.root
         for w in word:
-            i = self.charToIndex(w)
-            if i not in node.children: return False
-            node = node.children[i]
-        if node.isEnd: return True
-        return False
+            index = self.charToIndex(w)
+            if index not in node.children: return False
+            node = node.children[index]
+        return node.isEnd
 
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for w in prefix:
-            i = self.charToIndex(w)
-            if i not in node.children: return False
-            node = node.children[i]
+            index = self.charToIndex(w)
+            if index not in node.children: return False
+            node = node.children[index]
         return True
 
 # Your Trie object will be instantiated and called as such:
