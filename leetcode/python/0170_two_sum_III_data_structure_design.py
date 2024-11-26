a09c98714c5ec48ce0d2848502293dfca2d4ffa5
tahmid-saj <tahmid.saj@gmail.com>
2024-11-26 15:29:53 -0500

    lc added

---
+++ leetcode/python/0170_two_sum_III_data_structure_design.py
@@ -0,0 +1,21 @@
+class TwoSum:
+
+    def __init__(self):
+        self.seen = {}
+
+    def add(self, number: int) -> None:
+        self.seen[number] = self.seen.get(number, 0) + 1
+
+    def find(self, value: int) -> bool:
+        for k, v in self.seen.items():
+            complement = value - k
+            if complement == k and v >= 2: return True
+            elif complement == k and v < 2: continue
+
+            if complement in self.seen: return True
+        return False
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
