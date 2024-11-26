f5f692d789927f5da283df2a9d5d1b02b087e893
tahmid-saj <tahmid.saj@gmail.com>
2024-11-26 16:21:41 -0500

    lc 211 updated

--- leetcode/python/0211_design_add_and_search_words_data_structure.py
+++ leetcode/python/0211_design_add_and_search_words_data_structure.py
@@ -1,43 +1,41 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.isEnd = False  
+        self.isEnd = False
 
 class WordDictionary:
-
     def __init__(self):
         self.root = TrieNode()
-
+    
     def charToIndex(self, ch):
         return ord(ch) - ord('a')
 
     def addWord(self, word: str) -> None:
         node = self.root
         for w in word:
-            i = self.charToIndex(w)
-            if i not in node.children: node.children[i] = TrieNode()
-            node = node.children[i]
+            index = self.charToIndex(w)
+            if index not in node.children: node.children[index] = TrieNode()
+            node = node.children[index]
         node.isEnd = True
 
     def search(self, word: str) -> bool:
         node = self.root
-        return self.searchDfs(word, node, 0)
-    
-    def searchDfs(self, word, node, index):
-        if index == len(word) and node.isEnd == True: return True
-        if index == len(word) and node.isEnd == False: return False
-        if node == False: return False
+        if self.searchDFS(word, node, 0): return True
+        return False
+
+    def searchDFS(self, word, node, index):
+        if index >= len(word) and node.isEnd: return True
+        elif index >= len(word): return False
+        if not node: return False
+        
+        idx = self.charToIndex(word[index])
+
+        if word[index] != "." and idx not in node.children: return False
+        elif word[index] != "." and idx in node.children and self.searchDFS(word, node.children[idx], index + 1): return True
 
         if word[index] == ".":
-            # try all possible paths
             for k, v in node.children.items():
-                if self.searchDfs(word, v, index + 1): return True
-        else:
-            i = self.charToIndex(word[index])
-            if i not in node.children: return False
-            if self.searchDfs(word, node.children[i], index + 1): return True
-        
-        return False
+                if self.searchDFS(word, v, index + 1): return True
 
 
 # Your WordDictionary object will be instantiated and called as such:
