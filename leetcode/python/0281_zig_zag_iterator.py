07f5a1bb041df7ee9179c8463d0413c09214504b
tahmid-saj <tahmid.saj@gmail.com>
2024-11-27 15:01:32 -0500

    lc 281 updated

--- leetcode/python/0281_zig_zag_iterator.py
+++ leetcode/python/0281_zig_zag_iterator.py
@@ -1,44 +1,28 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
-        self.i = -1
-        self.j = -1
-        self.curr = None
         self.v1 = v1
         self.v2 = v2
-
-    def next(self) -> int:
-        if self.curr == 0: res = self.v1[self.i]
-        else: res = self.v2[self.j]
-        return res
+        self.i = 0
+        self.j = 0
+        self.currVec = 0 if v1 else 1
 
     def hasNext(self) -> bool:
-        self.correctPosition()
-        if self.curr == None: return False
-        elif self.curr == 0: return self.i < len(self.v1)
-        return self.j < len(self.v2)
-    
-    def correctPosition(self):
-        if self.curr == None:
-            if len(self.v1) == 0 and len(self.v2) == 0: self.curr = None
-            elif len(self.v1) == 0: 
-                self.curr = 1
-                self.j += 1
-            elif len(self.v2) == 0: 
-                self.curr = 0
-                self.i += 1
-            else: 
-                self.curr = 0
-                self.i += 1
-        elif self.curr == 0:
-            if self.j != len(self.v2) - 1:
-                self.curr = 1
-                self.j += 1
-            else: self.i += 1
-        elif self.curr == 1:
-            if self.i != len(self.v1) - 1:
-                self.curr = 0
-                self.i += 1
-            else: self.j += 1
+        if self.currVec == 0 and self.i < len(self.v1): return True
+        elif self.currVec == 1 and self.j < len(self.v2): return True
+        return False
+
+    def next(self) -> int:
+        res = None
+        if self.currVec == 0: 
+            res = self.v1[self.i]
+            self.i += 1
+            self.currVec = 1 if self.j < len(self.v2) else self.currVec
+        elif self.currVec == 1:
+            res = self.v2[self.j]
+            self.j += 1
+            self.currVec = 0 if self.j < len(self.v1) else self.currVec
+        
+        return res
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
