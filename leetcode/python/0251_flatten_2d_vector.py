bea9a7242894bb611bbead933981b768b107035b
tahmid-saj <tahmid.saj@gmail.com>
2024-11-27 02:08:37 -0500

    lc 251

--- leetcode/python/0251_flatten_2d_vector.py
+++ leetcode/python/0251_flatten_2d_vector.py
@@ -1,35 +1,26 @@
 class Vector2D:
-
     def __init__(self, vec: List[List[int]]):
-        # self.vec = []
-        # self.i = 0
-        # for item in vec:
-        #     if item is int: self.vec.append(item)
-        #     else:
-        #         for val in item: self.vec.append(val)
-        self.i = 0
-        self.j = 0
         self.vec = vec
+        self.j = 0
+        self.i = -1
+        self.moveToNextValid1DVec()
 
     def next(self) -> int:
-        # res = self.vec[self.i]
-        # self.i += 1
-        # return res
-        self.correctPosition()
         res = self.vec[self.i][self.j]
-        self.j += 1
+        if self.j + 1 < len(self.vec[self.i]): self.j += 1
+        else:
+            self.moveToNextValid1DVec()
+            self.j = 0
+
         return res
 
     def hasNext(self) -> bool:
-        # if self.i == len(self.vec): return False
-        # return True
-        self.correctPosition()
-        return self.i < len(self.vec)
+        if self.i < len(self.vec) and self.j < len(self.vec[self.i]): return True
+        else: return False
     
-    def correctPosition(self):
-        while self.i < len(self.vec) and self.j == len(self.vec[self.i]):
-            self.i += 1
-            self.j = 0
+    def moveToNextValid1DVec(self):
+        self.i += 1
+        while self.i < len(self.vec) and len(self.vec[self.i]) == 0: self.i += 1
 
 # Your Vector2D object will be instantiated and called as such:
 # obj = Vector2D(vec)
