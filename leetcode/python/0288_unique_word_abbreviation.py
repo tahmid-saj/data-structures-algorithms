0ad1006d32dec343e99bf41ac83a8f4239cf4756
tahmid-saj <tahmid.saj@gmail.com>
2024-11-27 15:18:02 -0500

    lc 288 updated

--- leetcode/python/0288_unique_word_abbreviation.py
+++ leetcode/python/0288_unique_word_abbreviation.py
@@ -1,19 +1,21 @@
+from collections import defaultdict
 class ValidWordAbbr:
-
     def __init__(self, dictionary: List[str]):
-        self.abbreviation = defaultdict(set)
-        for word in dictionary: self.abbreviation[self.abbreviate(word)].add(word)
-    
-    def abbreviate(self, word):
-        if len(word) <= 2: return word
-        return word[0] + str(len(word) - 2) + word[-1]
+        self.abb = defaultdict(set)
+        for word in dictionary: 
+            abb = self.abbreviate(word)
+            self.abb[abb].add(word)
 
     def isUnique(self, word: str) -> bool:
-        abbreviation = self.abbreviate(word)
-        if abbreviation in self.abbreviation:
-            if word in self.abbreviation[abbreviation] and len(self.abbreviation[abbreviation]) == 1: return True
-            return False
-        return True
+        abb = self.abbreviate(word)
+        if abb not in self.abb: return True
+        if abb in self.abb and word in self.abb[abb] and len(self.abb[abb]) == 1: return True
+        return False
+
+    def abbreviate(self, word):
+        if len(word) <= 2: return word
+        return word[0] + str(len(word[1:-1])) + word[-1]
+
 
 # Your ValidWordAbbr object will be instantiated and called as such:
 # obj = ValidWordAbbr(dictionary)
