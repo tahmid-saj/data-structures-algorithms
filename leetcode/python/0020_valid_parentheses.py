c4bf00e0835c1855bec277f8f84b334d41662752
tahmid-saj <tahmid.saj@gmail.com>
2024-11-27 16:04:41 -0500

    lc 20 updated

--- leetcode/python/0020_valid_parentheses.py
+++ leetcode/python/0020_valid_parentheses.py
@@ -1,22 +1,10 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         stk = []
-        # traverse through s using i:
-        # -> if s[i] == "(" or s[i] == "[" or s[i] == "{": stk.append(s[i])
-        # -> elif s[i] == ")" and stk[-1] == "(": stk.pop()
-        # -> elif s[i] == "]" and stk[-1] == "[": stk.pop()
-        # -> elif s[i] == "}" and stk[-1] == "{": stk.pop()
-        # -> else: return False
-        # return True
-        for i in range(len(s)):
-            if s[i] == "(" or s[i] == "[" or s[i] == "{": stk.append(s[i])
-            elif (s[i] == ")" or s[i] == "]" or s[i] == "}") and not stk: return False 
-            elif s[i] == ")" and stk[-1] == "(": stk.pop()
-            elif s[i] == "]" and stk[-1] == "[": stk.pop()
-            elif s[i] == "}" and stk[-1] == "{": stk.pop()
+        for p in s:
+            if p in "([{": stk.append(p)
+            elif p == ")" and stk and stk[-1] == "(": stk.pop()
+            elif p == "]" and stk and stk[-1] == "[": stk.pop()
+            elif p == "}" and stk and stk[-1] == "{": stk.pop()
             else: return False
-        return not stk
+        return len(stk) == 0
