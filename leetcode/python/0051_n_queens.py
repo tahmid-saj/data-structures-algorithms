66c7eea6c4750bae4b8c802db98f077c79546839
tahmid-saj <tahmid.saj@gmail.com>
2024-12-01 01:57:42 -0500

    lc 51 updated

--- leetcode/python/0051_n_queens.py
+++ leetcode/python/0051_n_queens.py
@@ -13,19 +13,19 @@ class Solution:
         for col in range(n):
             diag, antiDiag = row - col, row + col
             if (col in cols) or (diag in diags) or (antiDiag in antiDiags): continue
-            
+
             cols.add(col)
             diags.add(diag)
             antiDiags.add(antiDiag)
             board[row][col] = "Q"
 
             self.backtrack(n, row + 1, cols, diags, antiDiags, board)
-            
+
             cols.remove(col)
             diags.remove(diag)
             antiDiags.remove(antiDiag)
             board[row][col] = "."
-    
+
     def format(self, board):
         res = []
         for row in board: res.append("".join(row))
