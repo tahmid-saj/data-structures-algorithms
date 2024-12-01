cc74a45154a3d381aa726d0ae173b594706121e9
tahmid-saj <tahmid.saj@gmail.com>
2024-11-30 19:57:42 -0500

    lc 359 updated

--- leetcode/python/0359_logger_rate_limiter.py
+++ leetcode/python/0359_logger_rate_limiter.py
@@ -1,18 +1,14 @@
 class Logger:
 
     def __init__(self):
-        self.logs = {}
-        self.time = 10
+        self.logs = defaultdict(int)
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.logs: 
-            self.logs[message] = timestamp
-            return True
+        if message not in self.logs: self.logs[message] = timestamp + 10
         else:
-            if timestamp - self.logs[message] >= self.time:
-                self.logs[message] = timestamp
-                return True
-        return False
+            if timestamp < self.logs[message]: return False
+            self.logs[message] = timestamp + 10
+        return True
 
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
