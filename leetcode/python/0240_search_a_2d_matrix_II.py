88501d67de47890752415f40914ca523f724469a
tahmid-saj <tahmid.saj@gmail.com>
2024-12-01 00:49:38 -0500

    lc 240 updated

--- leetcode/python/0240_search_a_2d_matrix_II.py
+++ leetcode/python/0240_search_a_2d_matrix_II.py
@@ -3,12 +3,10 @@ class Solution:
         return self.binarySearchConstantSpace(matrix, target)
 
     def binarySearchConstantSpace(self, matrix, target):
-        row, col = len(matrix) - 1, 0
-        while row < len(matrix) and row >= 0 and col < len(matrix[0]) and col >= 0:
-            middle = matrix[row][col]
-
-            if middle == target: return True
-            elif middle < target: col += 1
-            else: row -= 1
-        
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and i < len(matrix) and j >= 0 and j < len(matrix[0]):
+            val = matrix[i][j]
+            if val == target: return True
+            elif val < target: j += 1
+            else: i -= 1
         return False
