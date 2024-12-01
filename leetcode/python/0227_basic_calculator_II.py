9eb1532bf182388e917c32d1700c8dbc221d9740
tahmid-saj <tahmid.saj@gmail.com>
2024-12-01 01:35:23 -0500

    lc 227 updated

--- leetcode/python/0227_basic_calculator_II.py
+++ leetcode/python/0227_basic_calculator_II.py
@@ -1,23 +1,22 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # return self.stack(s)
-        return self.withoutStack(s)
+        return self.stack(s)
+        # return self.withoutStack(s)
     
     def stack(self, s):
-        if not s or len(s) == 0: return 0
-        stk, num, operation = [], 0, "+"
+        if not s: return 0
+        stk, num, op = [], 0, "+"
 
         for i in range(len(s)):
-            curr = s[i]
-            if curr.isnumeric(): num = (num * 10) + int(curr)
-            if (not curr.isnumeric() and curr != " ") or i == len(s) - 1:
-                # operation is the previous operator, not the current
-                if operation == "-": stk.append(-num)
-                elif operation == "+": stk.append(num)
-                elif operation == "*": stk.append(stk.pop() * num)
-                elif operation == "/": stk.append(int(stk.pop() / num))
-                operation = curr
+            if s[i].isnumeric(): num = (num * 10) + int(s[i])
+            if (not s[i].isnumeric() and s[i] != " ") or i == len(s) - 1:
+                # op refers to the previous operation, not the current one
+                if op == "+": stk.append(num)
+                elif op == "-": stk.append(-num)
+                elif op == "/": stk.append(int(stk.pop() / num))
+                elif op == "*": stk.append(stk.pop() * num)
                 num = 0
+                op = s[i]
         
         res = 0
         while stk: res += stk.pop()
